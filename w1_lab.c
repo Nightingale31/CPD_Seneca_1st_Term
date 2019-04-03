90dd4f2eaf31a5471b5374ff1f06eac4e84ceed2
Nightingale31 <47093410+Nightingale31@users.noreply.github.com>
2019-04-03 13:05:40 -0400

    Add files via upload

---
+++ w1_lab.c
@@ -0,0 +1,18 @@
+// Name: Artem Solovev
+// Student#: 136267184
+// Email: asolovev@myseneca.ca
+// Cource: IPC144
+// Section: J
+// Date: 1/18/2019
+
+// This program just outputs the text from the Workshop1 - *** Welcome to C Programming ***
+
+
+
+#include <stdio.h>  // This is the C standard libary header
+
+int main()
+{
+	printf("*** Welcome to C Programming ***\n");   // printf is a function that prints string, float, character and other values 
+	return 0;
+}
